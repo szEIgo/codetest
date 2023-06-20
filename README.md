8b98157d42954b69e142abbe5998f7c8d78913d9
Jonathan Szigethy <josz@nuuday.dk>
2023-06-20 08:27:16 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
-# codetest
+# Coding Test for a Responsible Company
+
+Prerequisites:
+- postgres
+- scala
 
 
-Start up and empty database:
-
-podman run --rm  --net=host --name responsibly-db -e POSTGRES_PASSWORD=responsibly -d docker.io/postgres:14.8-bullseye
-    or
-docker run --rm  --net=host --name responsibly-db -e POSTGRES_PASSWORD=responsibly -d postgres:14.8-bullseye
-
-
-Run sbt test to populate the database with data, this is instead of using seeding to flyway.
-
-After tests has successfully executed, do 
-sbt run - which should bind an grpc server to a specified port.
-
-This project requires some environment variables to load and run, this env varialbes have been added the verySecret.env file, as this is not a sensitive project, and no harm will be done by publishing details.
+Stack:
+- Web,Concurrency = Akka
+- Rpc = Akka-Grpc
+- Dao = Doobie
+- Migration = Flyway
+- Test = Scalatest
+- Logger = airframe-log
 
 
-When the project is listening on a port, it should be possible for you to use any grpc client with the embedded protobuf files, to communicate and start using this server.
+
+Protobuf files:
+/src/main/proto/SustainabilityService.proto
 
 
- 
+How to run:
+- sbt test (also adds data to the database)
+- sbt run (updates database, and provides a grpc server on specified port)
+- verySecret.env file has been bundled with the project
+
+
+
+
+if you need a fast potsgres:
+```
+docker run --rm --net=host --name responsibly-db -e POSTGRES_PASSWORD=responsibly -d postgres:14.8-bullseye
+
+```
 
 
--- src/test/scala/CalculationTest.scala
+++ src/test/scala/CalculationTest.scala
@@ -1,6 +1,8 @@
 import model._
 import org.scalatest.funspec.AnyFunSpec
 
+import scala.collection.MapView
+
 class CalculationTest extends AnyFunSpec {
   val f = Fixture
   describe("it should be able to aggregate and calculate correct results") {
@@ -40,69 +42,56 @@ class CalculationTest extends AnyFunSpec {
       "Use the ScoreCalculator to check whether the results are exactly equal to what is shown in the assignment description, " +
         "with a weight being 1-1 Avg") {
 
+      val groupedEntries: Map[SupplierId, Map[ParameterId, Map[DataSourceId, List[DataEntry]]]] =
+        f.listOfEntries
+          .groupBy(_.supplierId)
+          .view
+          .mapValues(_.groupBy(_.parameterId).view.toMap.mapValues(_.groupBy(_.dataSourceId)).toMap)
+          .toMap
 
+      println(s"groupedEntries ${groupedEntries.view.toMap}")
 
-
-
-
-
-      val suppliers = f.listOfEntries
-        .groupBy(_.supplierId)
-        .map { case (supplierId, entries) =>
-          val parameterScores = entries
-            .groupBy(_.parameterId)
-            .collect {
-              case (parameterId, paramEntries) if paramEntries.exists(_.parameterId == parameterId) =>
-                paramEntries
-                  .groupBy(_.dataSourceId)
-                  .collect {
-                    case (dataSourceId, dataEntries) if dataEntries.exists(_.dataSourceId == dataSourceId) =>
-                      val map: Map[DataSourceId, BigDecimal] = dataEntries
-                        .collect {
-                          case DataEntry(_, _, zdataSourceId, _, zscore) if dataSourceId == zdataSourceId =>
-                            zdataSourceId -> zscore
-                        }.toMap
-                      DataSourceScores(map,map.values.sum/map.size)
-                  }
+      val dataSourceScores = groupedEntries
+        .mapValues(_.flatMap {
+          case (_, paramEntries) =>
+            paramEntries.map {
+              case (_, dataEntries) =>
+                val dataSourceId = dataEntries.head.dataSourceId
+                val totalScore = dataEntries.map(_.score).sum
+                dataSourceId -> totalScore
             }
+        })
 
+      println(s"dataSourceScores ${dataSourceScores.view.toMap}")
 
-//            .map( parameterScore => {
-//            val value: Map[DataSourceId, DataSourceScores] = parameterScore._2
-//            val sum: BigDecimal = parameterScore._2.values.map(_.aggregatedScore).sum
-//            parameterScore._1 -> value, sum)
-//            supplierId -> parameterScores
-//          }
+      val parameterScores: MapView[DataSourceId, BigDecimal] = dataSourceScores
+        .flatMap {
+          case (supplierId, data) =>
+            data.toList
+              .groupBy(_._1)
+              .view
+              .mapValues(_.map(_._2).sum)
+              .map {
+                case (parameterId, totalScore) =>
+                  parameterId -> totalScore
+              }
+              .toList
+              .sortBy(_._1.id)
         }
+        .groupBy(_._1)
+        .view
+        .mapValues(_.map(_._2).sum)
 
+      println(s"parameterScores ${parameterScores.view.toMap}")
 
+      val supplierScores: MapView[String, BigDecimal] = parameterScores.toList
+        .groupBy(_._1.id)
+        .view
+        .mapValues(_.map(_._2).sum)
 
-//      val suppliers = f.listOfEntries.groupBy(_.supplierId).map { tuple =>
-//        val value: immutable.Iterable[ParameterScores] = tuple._2.groupBy(_.parameterId).collect {
-//          case xObj@(paramId, entries) if entries.exists(_.parameterId == paramId) =>
-//            val parameterMapScores: Map[ParameterId, DataSourceScores] = entries.groupBy(_.dataSourceId).collect {
-//              case yObj@(dataSourceId, yEntries)
-//                if yEntries.exists(_.dataSourceId == dataSourceId) =>
-//                val dataSourceIdAndScores: Map[DataSourceId, BigDecimal] = yEntries.collect {
-//                  case DataEntry(_, _, zdataSourceId, _, zscore)
-//                    if dataSourceId == zdataSourceId =>
-//                    zdataSourceId -> zscore
-//                }.toMap
-//                paramId -> DataSourceScores(
-//                  dataSourceIdAndScores,
-//                  dataSourceIdAndScores.values.sum)
-//            }
-//            ParameterScores(parameterMapScores, parameterMapScores.values.map(_.aggregatedScore).sum)
-//        }
-//        println(s"øøøø$value")
-//        value
-//      }
-
-      SupplierScores(suppliers,)
-      println(s"#####$suppliers, \n #####${suppliers.size}")
+      println(s"supplierScores ${supplierScores.view.toMap}")
 
     }
-
   }
 
 }
